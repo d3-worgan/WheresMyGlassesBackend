6c0c7850b88c1f729225de5f6035fc22d351b6e2
d3-worgan <daniel2.worgan@live.uwe.ac.uk>
2019-12-12 22:36:48 +0000

    First commit, added verbaliser class with sentances

---
+++ WheresMyGlasses/source/voice_assistant/Verbaliser.py
@@ -0,0 +1,62 @@
+
+
+class Verbaliser():
+    def __init__(self):
+        print("Verbalising a message")
+
+    @staticmethod
+    def construct_location_message(located_object, location):
+        """
+        Construct a message describing where the required object is using the info
+        from the back end.
+        :param located_object: String, the name of object located
+        :param location: String, where the object is located or nearby
+        :return:
+        """
+        print("The " + located_object + " is by the " + location)
+        return "The " + located_object + " is by the " + location
+
+    @staticmethod
+    def construct_not_found_message(lost_object):
+        """
+        Construct a message explaining that the system could not find the specified
+        object.
+        :param lost_object: A String describing the lost object
+        :return:
+        """
+        print("The system could not find " + lost_object)
+        return "The system could not find " + lost_object
+
+    @staticmethod
+    def construct_unknown_object_message(unknown_object):
+        """
+        Construct a message explaining the system does not recognise that object
+        :param unknown_object:
+        :return:
+        """
+        print("Ive never heard of pizza pie before, but might be able to help you with something else")
+        return "Ive never heard of " + unknown_object + " before, but might be able to help you with something else"
+
+    @staticmethod
+    def construct_check_input_message(potential_object):
+        """
+        Construct a message explaining the system wants to confirm the object being searched for
+        :param potential_object:
+        :return:
+        """
+        print("I didnt quite hear that. Did you say you want to look for " + potential_object)
+        return "I didnt quite hear that. Did you say you want to look for " + potential_object
+
+    @staticmethod
+    def construct_bad_input_message():
+        """
+        Construct a message explaining that the system had a low confidence score in the
+        speech recognition.
+        :return:
+        """
+        print("I did not hear that properly, please ask again a bit louder")
+        return "I did not hear that properly, please ask again a bit louder"
+
+
+
+
---
+++ WheresMyGlasses/source/voice_assistant/VoiceAssistant.py
@@ -0,0 +1,17 @@
+
+
+
+class Assistant:
+    def __init__(self):
+        print("Hello I am the assistant")
+
+    def validate_user_input(self):
+        print("Validating the users message")
+
+    def verbalise_message(self):
+        print("Here is my message to you")
+
+    def sendSearchRequest(self):
+        print("Asking the object locater to search for your item")
+
+
