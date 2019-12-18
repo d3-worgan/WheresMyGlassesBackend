0feb2a908f803aebf51503618b4767f77a7843a1
d3-worgan <daniel2.worgan@live.uwe.ac.uk>
2019-12-18 21:05:30 +0000

    Set the object locator to intialise the object detector. Added a search room method to test

---
+++ WheresMyGlasses/source/search_assistant/ObjectLocator.py
@@ -0,0 +1,146 @@
+from WheresMyGlasses.source.search_assistant.Snapshot import Snapshot
+from WheresMyGlasses.source.search_assistant.DetectedObject import ObjectDetected
+
+import cv2
+import numpy as np
+
+
+class ObjectLocator:
+    """
+    The object locator searches the room for objects and attempts to generate some
+    location information for each object.
+
+    The voice interface can contact the object locator to search the room, and then tell the
+    user what the object locator found.
+
+    """
+
+    def __init__(self):
+        """
+        Initialise the object locator by loading the object detector
+        """
+        print("Preparing object locator...")
+
+        # Save a number of snapshots to search for object in memory
+        self.snapshot_frequency = 10
+        self.snapshot_buffer_size = 1440
+        self.snapshot_history = []
+
+        # Load the neural network
+        print("Initalizing YOLO...")
+        self.net = cv2.dnn.readNetFromDarknet("yolov3.cfg", "yolov3.weights")
+
+        # Load the class names
+        print("loading class names...")
+        self.classes = []
+        with open("coco.names", "r") as f:
+            self.classes = [line.strip() for line in f.readlines()]
+
+        # Build the network
+        print("Building Neural Network...")
+        self.layer_names = self.net.getLayerNames()
+        self.output_layers = [self.layer_names[i[0] - 1] for i in self.net.getUnconnectedOutLayers()]
+        self.colors = np.random.uniform(0, 255, size=(len(self.classes), 3))
+
+        # Open the camera stream
+        print("Reading camera stream...")
+        self.video_capture = cv2.VideoCapture(0)
+        if not self.video_capture.isOpened():
+            raise Exception("Could not open video device")
+
+        print("Object locator ready.")
+
+    def search_room(self):
+
+        # Read an image from the camera
+        ret, img = self.video_capture.read()
+        height, width, channels = img.shape
+
+        # Process the image and run through YOLO
+        print("Object detection.")
+        blob = cv2.dnn.blobFromImage(img, 0.00392, (416, 416), (0, 0, 0), True, crop=False)
+        self.net.setInput(blob)
+        outs = self.net.forward(self.output_layers)
+
+        # Store information we extract from the image
+        class_ids = []
+        confidences = []
+        boxes = []
+        centers = []
+
+        # Extract detections and information from the image
+        for out in outs:
+            for detection in out:
+                scores = detection[5:]
+                class_id = np.argmax(scores)
+                confidence = scores[class_id]
+                if confidence > 0.5:
+                    label = self.classes[class_id]
+                    # Object detected
+                    center_x = int(detection[0] * width)
+                    center_y = int(detection[1] * height)
+                    w = int(detection[2] * width)
+                    h = int(detection[3] * height)
+                    # Rectangle coordinates
+                    x = int(center_x - w / 2)
+                    y = int(center_y - h / 2)
+                    boxes.append([x, y, w, h])
+                    confidences.append(float(confidence))
+                    class_ids.append(class_id)
+                    centers.append([center_x, center_y])
+
+        # Reduces double detections and errors
+        indexes = cv2.dnn.NMSBoxes(boxes, confidences, 0.5, 0.4)
+
+        # Finalize a list of detected objects
+        detected_objects = []
+        for i in range(len(boxes)):
+            if i in indexes:
+                detected_objects.append(
+                    ObjectDetected(class_ids[i], self.classes[class_ids[i]], confidences[i], centers[i][0],
+                                   centers[i][1], boxes[i][0], boxes[i][1], boxes[i][2], boxes[i][3]))
+
+        # Draw bounding boxes on detected objects
+        font = cv2.FONT_HERSHEY_PLAIN
+        color = (0, 255, 0)
+        for obj in detected_objects:
+            cv2.rectangle(img, (obj.x, obj.y), (obj.x + obj.w, obj.y + obj.h), color, 2)
+            cv2.putText(img, obj.label, (obj.x, obj.y + 30), font, 3, color, 3)
+
+        # Locate items that are close to each other
+        for obj in detected_objects:
+            for loc in detected_objects:
+                if loc.cid != obj.cid:
+                    if loc.x < obj.center_x < (loc.x + loc.w) and loc.y < obj.center_y < (loc.y + loc.h):
+                        print("The " + obj.label + " is by the " + loc.label)
+                        cv2.circle(img, (obj.center_x, obj.center_y), 20, (255, 0, 0), 3)
+
+        # Display results
+        cv2.imshow("Image", img)
+
+        # Finish up
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+        print("Done.")
+
+
+    def take_snapshot(self):
+        """
+        Takes a snapshot and saves it to the snapshot history, maintains a specified
+        number of snapshots
+        :return:
+        """
+        print("Taking a snapshot")
+        if len(self.snapshot_history) > self.snapshot_buffer_size:
+            self.snapshot_history.pop(0)
+
+        self.snapshot_history.append(Snapshot())
+
+    def find_snapshot(self):
+        print("Searching for items...")
+
+
+
+ol = ObjectLocator()
+
+ol.search_room()
