34861d3877fae9a45e58342ba0c6ac0dbd6ada04
d3-worgan <daniel2.worgan@live.uwe.ac.uk>
2019-12-20 05:06:37 +0000

    Updated search history test, added extra info to LocatedObject, updated locate object and take snapshot

--- WheresMyGlasses/source/Tests/LocateObjectHistoryTest.py
+++ WheresMyGlasses/source/Tests/LocateObjectHistoryTest.py
@@ -1,34 +1,26 @@
 from WheresMyGlasses.source.search_assistant.ObjectLocator import ObjectLocator
 from WheresMyGlasses.source.voice_assistant.MessageBuilder import MessageBuilder
-import keyboard
 
 
+# TEST/DEMO
+# Capture some samples
+# Choose an item to request
+# Search the room or the history to see if the object was located
+
+# System running
 ol = ObjectLocator(100)
-requested_object = "bottle"
-
-pair = None
-message = None
-
-# while not found
-# maintain a list of 100 snapshots
-# Show an object to locate e.g. bottle on the dining table
-# When the 100 snapshots are done it will search back through and find it
-
-found = False
-pair = None
 i = 0
-
 while i < 100:
     ol.add_snapshot_to_history(i)
     i += 1
 
+# 'Received request'
+requested_object = "bottle"
+
+# Process request
 pair = ol.locate_object(requested_object)
-message = None
 
-if pair is not None:
-    print(pair.object1)
-    print(pair.object2)
-
+# Output
 if pair is None:
     message = MessageBuilder.construct_not_found_message(requested_object)
 else:
