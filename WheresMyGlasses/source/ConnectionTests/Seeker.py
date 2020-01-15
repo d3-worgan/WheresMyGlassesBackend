4425de832da0df076f836bebe734a3fd2484cbf9
d3-worgan <daniel2.worgan@live.uwe.ac.uk>
2020-01-15 21:23:23 +0000

    updated object code identification flow

--- WheresMyGlasses/source/ConnectionTests/Seeker.py
+++ WheresMyGlasses/source/ConnectionTests/Seeker.py
@@ -8,13 +8,6 @@ import paho.mqtt.client as mqtt
 import threading
 import time
 
-def validate_object(m_decode, ol):
-    for name in ol.object_detector.classes:
-        print("Searching names file for requested object ", m_decode)
-        if name == m_decode:
-            print("Object is in training data")
-            return True
-
 
 def on_log(client, userdata, level, buf):
     print("log: " + buf)
@@ -35,8 +28,8 @@ def on_message(client, userdata, msg):
     """
     Main callback function for when a request is received from the front end. First validate
     the detector can recognise the specified object, then take a snapshot. If the
-    object is not the snapshot check in the snapshot history. Publish a message to the
-    front end describing the results i.e. code located, code not found, code bad object.
+    object is not the snapshot check in the snapshot history. Produce a message code and
+    build a response object to send to the front end.
     :return: Publish a BackendResponse on the MQTT
     """
     # Decode the incoming message
@@ -50,36 +43,50 @@ def on_message(client, userdata, msg):
 
     lock.acquire()
     response = None
+    current_snapshot = None
+    object_located = False
+    message_code = ''
 
     if validate_object(m_decode, ol):
         # Use the object detector to see if the object is in the room
-        snapshot = ol.take_snapshot('x')
+        current_snapshot = ol.take_snapshot('x')
         print("Searching snapshot...")
-        locations_identified = ol.search_snapshot(snapshot, m_decode)
+        locations_identified = ol.search_snapshot(current_snapshot, m_decode)
         if len(locations_identified) == 1:
             print("Message code 1")
-            response = BackendResponse('1', m_decode, snapshot.timestamp, locations_identified)
-            print(response)
+            object_located = True
+            message_code = '1'
         elif len(locations_identified) > 1:
-                print("Message code 2")
-                response = BackendResponse('2', m_decode, snapshot.timestamp, locations_identified)
-                print(response)
+            print("Message code 2")
+            object_located = True
+            message_code = '2'
         else:
             print(f"The {m_decode} was not in the snapshot, searching the snapshot history...")
             for snap in snapshot_history:
                 print(f"Searching snapshot {snap.id}")
                 locations_identified = ol.search_snapshot(snap, m_decode)
                 if len(locations_identified) == 1:
-                    response = BackendResponse('3', m_decode, snap.timestamp, locations_identified)
+                    object_located = True
+                    message_code = '3'
+                    current_snapshot = snap
                 elif len(locations_identified) > 1:
-                    response = BackendResponse('4', m_decode, snapshot.timestamp, locations_identified)
-                else:
-                    print(f"The {m_decode} was not in the snapshots, returning not found...")
-                    response = BackendResponse('5', m_decode, None, [])
+                    object_located = True
+                    message_code = '4'
+                    current_snapshot = snap
+
+        if not object_located:
+            print("The object was not found, message code 5")
+            message_code = '5'
     else:
         # Inform the user the detector does not recognise that object
         print("The object has not been trained on the network, returning bad item")
-        response = BackendResponse('6', m_decode, None, [])
+        object_located = False
+        message_code = '6'
+
+    if object_located:
+        response = BackendResponse(message_code, m_decode, current_snapshot.timestamp, ol.search_snapshot(current_snapshot, m_decode))
+    else:
+        response = BackendResponse(message_code, m_decode, None, [])
 
     if response:
         response = response.pack()
@@ -92,6 +99,10 @@ def on_message(client, userdata, msg):
 
 
 def take_snapshots():
+    """
+    Background loop taking regular snapshots to keep track of objects and state of the room.
+    :return:
+    """
     i = 0
     while True:
         lock.acquire()
@@ -110,35 +121,45 @@ def take_snapshots():
         lock.release()
         time.sleep(0.1)
 
-print("Start")
-broker = "192.168.0.27"
-client = mqtt.Client("seeker")
 
-history_size = 1000
-snapshot_history = []
-ol = ObjectLocator()
-lock = threading.Lock()
-
-client.on_connect = on_connect
-client.on_log = on_log
-client.on_disconnect = on_disconnect
-client.on_message = on_message
-
-print("Connecting to broker ", broker)
-client.connect(broker)
-client.loop_start()
-client.subscribe("voice_assistant/user_requests")
+def validate_object(m_decode, ol):
+    for name in ol.object_detector.classes:
+        print("Searching names file for requested object ", m_decode)
+        if name == m_decode:
+            print("Object is in training data")
+            return True
 
 
-print("Saving snapshots to history...")
+if __name__ == "__main__":
+    print("Start")
+    broker = "192.168.0.27"
+    client = mqtt.Client("Backend")
 
-try:
-    take_snapshots()
-except KeyboardInterrupt:
-    print("User quit.")
-finally:
-    print("done")
-    lock.release()
-    client.loop_stop()
-    client.disconnect()
+    history_size = 1000
+    snapshot_history = []
+    ol = ObjectLocator()
+    lock = threading.Lock()
+
+    client.on_connect = on_connect
+    client.on_log = on_log
+    client.on_disconnect = on_disconnect
+    client.on_message = on_message
+
+    print("Connecting to broker ", broker)
+    client.connect(broker)
+    client.loop_start()
+    client.subscribe("voice_assistant/user_requests")
+
+
+    print("Saving snapshots to history...")
+
+    try:
+        take_snapshots()
+    except KeyboardInterrupt:
+        print("User quit.")
+    finally:
+        print("done")
+        lock.release()
+        client.loop_stop()
+        client.disconnect()
 
