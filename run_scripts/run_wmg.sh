377845e2e7c58ceddb2779bc9bcb907f753695da
d3-worgan <daniel2.worgan@live.uwe.ac.uk>
2020-03-18 00:15:16 +0000

    Tidy structure & gitignore

--- run_scripts/run_wmg.sh
+++
@@ -1,9 +0,0 @@
-source wmg_env/bin/activate
-#pip install -r requirements.txt
-
-# Run in linux
-python3 backend_manager.py --model "yolov3"
-
-# Run in windows (not implemented)
-#python3 backend_manager.py --model "yolov3" --location "E:\DetectionModels"
-
